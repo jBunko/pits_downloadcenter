a3c5931e8376fb49a4075701f152761d6fc9c232
Bunkowski Johann <johann.bunkowski@horsch.com>
2022-08-08 12:05:50 +0200

    initiial commit and plugin registration fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,38 @@
 <?php
-/***************************************************************************
+
+/***************************************************************
  * Extension Manager/Repository config file for ext "pits_downloadcenter".
- *********** ***************************************************************/
+ *
+ * Auto generated 11-07-2022 11:07
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Download Center',
-	'description' => 'Download Center Sponsored by TNT-Graphics AG',
-	'category' => 'plugin',
-	'version' => '4.0.3',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearcacheonload' => true,
-	'author' => 'Hoja M.A, Sruthi K',
-	'author_email' => 'hoja.ma@pitsolutions.com',
-	'author_company' => NULL,
-	'constraints' => array (
-		'depends' =>
-		array (
-			'typo3' => '7.6.0 - 10.9.99',
-		),
-		'conflicts' => array (),
-		'suggests' => array (),
-	),
+  'title' => 'Download Center',
+  'description' => 'Download Center Sponsored by TNT-Graphics AG',
+  'category' => 'plugin',
+  'version' => '4.0.3',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => true,
+  'author' => 'Hoja M.A, Sruthi K',
+  'author_email' => 'hoja.ma@pitsolutions.com',
+  'author_company' => NULL,
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '10.4.0 - 11.9.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
 );
 
